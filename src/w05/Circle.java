e2ee920ee8acc14ff4a3150cb664ca7c9220e890
Rafael Uttinger <rafael.uttinger@stud.hslu.ch>
2022-10-20 13:49:31 +0200

    feat(w05): add shape classes

---
+++ src/w05/Circle.java
@@ -0,0 +1,20 @@
+package src.w05;
+
+public class Circle extends Shape {
+    private int radius;
+
+    public Circle(final int x, final int y, final int radius) {
+        super(x, y);
+        this.radius = radius;
+    }
+
+    @Override
+    public int getPerimeter() {
+        return (int) (2*radius*Math.PI);
+    }
+
+    @Override
+    public int getArea() {
+        return (int) (Math.pow(radius, 2)*Math.PI);
+    }
+}
