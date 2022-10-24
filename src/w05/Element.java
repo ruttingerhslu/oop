637810c2a8f70e55370ed1db4d2ade15c6139ba0
Rafael Uttinger <rafael.uttinger@stud.hslu.ch>
2022-10-24 13:42:12 +0200

    feat(w05): add Element.java from legacy

---
+++ src/w05/Element.java
@@ -0,0 +1,15 @@
+package src.w05;
+/** Represents a chemical element.
+ * @author Rafael Uttinger
+ * @version 1.0
+*/
+public abstract class Element {
+
+    public Element(double boilingPoint, double meltingPoint) {}
+
+    public abstract double getBoilingPoint();
+
+    public abstract double getMeltingPoint();
+
+    public abstract String getStateOfMatter(double celsius);
+}
