09d8af6ec6706ad46cb72be6fb986a58fe9b1f86
Rafael Uttinger <rafael.uttinger@stud.hslu.ch>
2022-10-31 13:14:17 +0100

    test(legacy): add DemoTest

---
+++ test/main/legacy/DemoTest.java
@@ -0,0 +1,21 @@
+package test.main.legacy;
+
+import org.junit.jupiter.api.Test;
+import src.main.legacy.Demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DemoTest {
+
+    private final Demo demo = new Demo();
+
+    @Test
+    void testMaxPositive() {
+        assertEquals(demo.max(1, 2), 2);
+    }
+
+    @Test
+    void testMinPositive() {
+        assertEquals(demo.min(1, 2), 1); 
+    }
+}
